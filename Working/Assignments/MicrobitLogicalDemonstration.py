9d7a4b446188f92d6b00722236ea911401cab3be
justin-d-sudo <justin.lau22ycdsbk12.ca>
2019-10-31 09:45:47 -0400

    MicrobitLogivalDemonstration

--- Working/Assignments/MicrobitLogicalDemonstration.py
+++ Working/Assignments/MicrobitLogicalDemonstration.py
@@ -1,29 +1,27 @@
-from microbit import *
-
 #identify the pins
-crashsensor_pin = pin1
-soilmoisturesensor_pin = pin3
-miniservo_pin = pin8
-greenLED_pin = pin12
-redLED_pin = pin13
+Crashensor_pin = pin1
+moisturesensor_pin = pin3
+servo_pin = pin8
+LEDgreen_pin = pin12
+LEDgreen_pin = pin13
 
 #other variables
 healthwarning = false
 
 #if else statements
 while True: 
-    if soilmoisturesensor_pin.read_analog (moisture > 50) and (moisture < 100):
-        print("Your plant is good. Moisture level is %d" % soilmoisturesensor_pin.read_analog)
-        greenLED_pin.write_digital(12)
+    if moisturesensor_pin.read_analog (moisture > 50) and (moisture < 100):
+        print("Your plant is good. Moisture level is %d" % moisturesensor_pin.read_analog)
+        LEDgreen_pin.write_digital(12)
         display.show(Image.HAPPY)
         
     else:
-        print("Your plant needs water. Moisture level is %d" % soilmoisturesensor_pin.read_analog)
-        redLED_pin.write_digital(13)
+        print("Your plant needs water. Moisture level is %d" % moisturesensor_pin.read_analog)
+        LEDred_pin.write_digital(13)
         while True:
-            if crashsensor_pin.read_digital()== 0:
+            if Crashsensor_pin.read_digital()== 0:
                 servo.right_angle(180)
-                crashsensor_pin.read_analog(1)
+                Crashsensor_pin.read_analog(1)
                 display.show(Imaage.SAD)
 
 
