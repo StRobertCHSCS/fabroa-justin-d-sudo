b853a0be769dcf3aa3c2ef7e3e21d112634320ca
justin-d-sudo <justin.lau22ycdsbk12.ca>
2019-11-01 09:46:42 -0400

    Microbit Assignment

--- Working/Assignments/MicrobitLogicalDemonstration.py
+++ Working/Assignments/MicrobitLogicalDemonstration.py
@@ -1,27 +1,28 @@
-#identify the pins
+from microbit import *
+
+# identify the pins
 Crashensor_pin = pin1
 moisturesensor_pin = pin3
 servo_pin = pin8
 LEDgreen_pin = pin12
-LEDgreen_pin = pin13
+LEDred_pin = pin13
 
 #other variables
-healthwarning = false
+healthwarning = False 
 
 #if else statements
 while True: 
-    if moisturesensor_pin.read_analog (moisture > 50) and (moisture < 100):
+    if moisturesensor_pin.read_analog() >50 and moisturesensor_pin.read_analog() <75:
         print("Your plant is good. Moisture level is %d" % moisturesensor_pin.read_analog)
         LEDgreen_pin.write_digital(12)
-        display.show(Image.HAPPY)
+        crashsensor_pin.read_analog(0)
+
+    elif crashsensor_pin.read_digital()== 1:
+        servo.right_angle(180)
         
     else:
         print("Your plant needs water. Moisture level is %d" % moisturesensor_pin.read_analog)
         LEDred_pin.write_digital(13)
-        while True:
-            if Crashsensor_pin.read_digital()== 0:
-                servo.right_angle(180)
-                Crashsensor_pin.read_analog(1)
-                display.show(Imaage.SAD)
+                
 
 
