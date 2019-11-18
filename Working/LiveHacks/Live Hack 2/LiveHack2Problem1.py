dfb35263016a87c23c0bfc06fb9062185709c69c
justin-d-sudo <justin.lau22ycdsbk12.ca>
2019-11-18 09:21:10 -0500

    Live Hack 2 Problem

---
+++ Working/LiveHacks/Live Hack 2/LiveHack2Problem1.py
@@ -0,0 +1,25 @@
+'''
+-------------------------------------------------------------------------------
+Name:		LiveHack2Problem1.py
+Purpose:	Determine the BMI of the patient
+
+Author:		Lau.J
+
+Created:	18/11/2019
+-------------------------------------------------------------------------------1.73
+'''
+
+# GET the height and weight
+height = float(input("Enter your height in metres: "))
+weight = float(input("Enter your weight in kilograms: "))
+
+# COMPUTE the BMI(body mass index)
+bodymassindex = weight/height**2
+
+# OUTPUT the message to determine the health of patient
+if bodymassindex > 25:
+    print("Your BMI is more than twenty five which means you are overweight.")
+elif bodymassindex > 18.5 and bodymassindex < 25.0:
+    print("Your BMI is between eighteen and a half and twenty five so you are normal weight.")
+else:
+    print("Your BMI is less than eighteen and a half which means you are underweight.")
